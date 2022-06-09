6a2e6e70af60f7fa08608f5a10f7b1366e43af7f
Calvin <czd890@live.cn>
2022-06-09 15:20:17 +0800

    a

--- src/Extensions/src/App.Metrics.Extensions.Collectors/EventListeners/GcEventListener.cs
+++ src/Extensions/src/App.Metrics.Extensions.Collectors/EventListeners/GcEventListener.cs
@@ -4,11 +4,12 @@
 
 using System.Diagnostics.Tracing;
 using System.Linq;
+
 using App.Metrics.Extensions.Collectors.MetricsRegistries;
 
 namespace App.Metrics.Extensions.Collectors.EventListeners
 {
-     public class GcEventListener : EventListener
+    public class GcEventListener : EventListener
     {
         // https://docs.microsoft.com/en-us/dotnet/framework/performance/garbage-collection-etw-events
         private const int GC_KEYWORD = 0x0000001;
@@ -21,9 +22,10 @@ namespace App.Metrics.Extensions.Collectors.EventListeners
 
         protected override void OnEventSourceCreated(EventSource eventSource)
         {
+            // https://docs.microsoft.com/zh-cn/dotnet/core/diagnostics/well-known-event-providers
             if (eventSource.Name.Equals("Microsoft-Windows-DotNETRuntime"))
             {
-                EnableEvents(eventSource, EventLevel.Informational, (EventKeywords) GC_KEYWORD);
+                EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)GC_KEYWORD);
             }
         }
 
@@ -31,13 +33,16 @@ namespace App.Metrics.Extensions.Collectors.EventListeners
         {
             switch (eventData.EventName)
             {
+                case "GCHeapStats_V2 ":
+                    ProcessHeapStats(eventData, 2);
+                    break;
                 case "GCHeapStats_V1":
-                    ProcessHeapStats(eventData);
+                    ProcessHeapStats(eventData, 1);
                     break;
             }
         }
 
-        private void ProcessHeapStats(EventWrittenEventArgs eventData)
+        private void ProcessHeapStats(EventWrittenEventArgs eventData, int version)
         {
             if (eventData?.Payload == null || eventData.Payload.Count == 0)
             {
@@ -52,47 +57,37 @@ namespace App.Metrics.Extensions.Collectors.EventListeners
             ulong gen2Survived = 0;
             ulong lohSize = 0;
             ulong lohSurvived = 0;
+            ulong gen4HeapSize = 0;
+            ulong gen4Promoted = 0;
+
+            uint pinnedObjectCount = 0;
+            uint gCHandleCount = 0;
 
             if (eventData.Payload.ElementAtOrDefault(0) != null && eventData.Payload[0] != null)
-            {
                 gen0HeapSize = (ulong)eventData.Payload[0];
-            }
-            
             if (eventData.Payload.ElementAtOrDefault(1) != null && eventData.Payload[1] != null)
-            {
                 gen0Promoted = (ulong)eventData.Payload[1];
-            }
-            
+
             if (eventData.Payload.ElementAtOrDefault(2) != null && eventData.Payload[2] != null)
-            {
                 gen1HeapSize = (ulong)eventData.Payload[2];
-            }
-            
             if (eventData.Payload.ElementAtOrDefault(3) != null && eventData.Payload[3] != null)
-            {
                 gen1Promoted = (ulong)eventData.Payload[3];
-            }
-            
+
             if (eventData.Payload.ElementAtOrDefault(4) != null && eventData.Payload[4] != null)
-            {
                 gen2HeapSize = (ulong)eventData.Payload[4];
-            }
-            
             if (eventData.Payload.ElementAtOrDefault(5) != null && eventData.Payload[5] != null)
-            {
                 gen2Survived = (ulong)eventData.Payload[5];
-            }
-            
+
             if (eventData.Payload.ElementAtOrDefault(6) != null && eventData.Payload[6] != null)
-            {
                 lohSize = (ulong)eventData.Payload[6];
-            }
-            
             if (eventData.Payload.ElementAtOrDefault(7) != null && eventData.Payload[7] != null)
-            {
                 lohSurvived = (ulong)eventData.Payload[7];
-            }
-           
+
+            if (eventData.Payload.ElementAtOrDefault(10) != null && eventData.Payload[10] != null)
+                pinnedObjectCount = (uint)eventData.Payload[10];
+            if (eventData.Payload.ElementAtOrDefault(12) != null && eventData.Payload[12] != null)
+                gCHandleCount = (uint)eventData.Payload[12];
+
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.Gen0HeapSize, gen0HeapSize);
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.BytesPromotedFromGen0, gen0Promoted);
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.Gen1HeapSize, gen1HeapSize);
@@ -101,7 +96,20 @@ namespace App.Metrics.Extensions.Collectors.EventListeners
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.BytesSurvivedFromGen2, gen2Survived);
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.LargeObjectHeapSize, lohSize);
             _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.BytesSurvivedLargeObjectHeap, lohSurvived);
+            _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.PinnedObjects, pinnedObjectCount);
+            _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.Handles, gCHandleCount);
 
+
+            if (version == 2)
+            {
+                if (eventData.Payload.ElementAtOrDefault(14) != null && eventData.Payload[14] != null)
+                    gen4HeapSize = (ulong)eventData.Payload[14];
+                if (eventData.Payload.ElementAtOrDefault(15) != null && eventData.Payload[15] != null)
+                    gen4Promoted = (ulong)eventData.Payload[15];
+
+                _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.PinnedObjectHeapSize, gen4HeapSize);
+                _metrics.Measure.Gauge.SetValue(GcMetricsRegistry.Gauges.BytesSurvivedPinnedoObjectHeap, gen4Promoted);
+            }
         }
     }
 }
--- src/Extensions/src/App.Metrics.Extensions.Collectors/MetricsRegistries/GcMetricsRegistry.cs
+++ src/Extensions/src/App.Metrics.Extensions.Collectors/MetricsRegistries/GcMetricsRegistry.cs
@@ -14,7 +14,7 @@ namespace App.Metrics.Extensions.Collectors.MetricsRegistries
                 Name = "Gen 0 Collections",
                 MeasurementUnit = Unit.Events
             };
-            
+
             public static readonly GaugeOptions Gen1Collections = new GaugeOptions
             {
                 Context = ContextName,
@@ -28,7 +28,7 @@ namespace App.Metrics.Extensions.Collectors.MetricsRegistries
                 Name = "Gen 2 Collections",
                 MeasurementUnit = Unit.Events
             };
-        
+
             public static readonly GaugeOptions LiveObjectsSize = new GaugeOptions
             {
                 Context = ContextName,
@@ -42,55 +42,81 @@ namespace App.Metrics.Extensions.Collectors.MetricsRegistries
                 Name = "Gen 0 Heap Size",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions Gen1HeapSize = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Gen 1 Heap Size",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions Gen2HeapSize = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Gen 2 Heap Size",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions LargeObjectHeapSize = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Large Object Heap Size",
                 MeasurementUnit = Unit.Bytes
             };
-            
+            public static readonly GaugeOptions PinnedObjectHeapSize = new GaugeOptions
+            {
+                Context = ContextName,
+                Name = "Pinned Object Heap Size",
+                MeasurementUnit = Unit.Bytes
+            };
+
             public static readonly GaugeOptions BytesPromotedFromGen0 = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Bytes Promoted From Gen 0",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions BytesPromotedFromGen1 = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Bytes Promoted From Gen 1",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions BytesSurvivedFromGen2 = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Bytes Survived From Gen 2",
                 MeasurementUnit = Unit.Bytes
             };
-            
+
             public static readonly GaugeOptions BytesSurvivedLargeObjectHeap = new GaugeOptions
             {
                 Context = ContextName,
                 Name = "Bytes Survived Large Object Heap",
                 MeasurementUnit = Unit.Bytes
             };
+            public static readonly GaugeOptions BytesSurvivedPinnedoObjectHeap = new GaugeOptions
+            {
+                Context = ContextName,
+                Name = "Bytes Survived Pinned Object Heap",
+                MeasurementUnit = Unit.Bytes
+            };
+
+            public static readonly GaugeOptions PinnedObjects = new GaugeOptions
+            {
+                Context = ContextName,
+                Name = "Pinned Objects",
+                MeasurementUnit = Unit.Items
+            };
+            public static readonly GaugeOptions Handles = new GaugeOptions
+            {
+                Context = ContextName,
+                Name = "Handles Count",
+                MeasurementUnit = Unit.Items
+            };
+
         }
     }
 }
